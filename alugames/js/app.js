421c8bfc7753a0acb7037cc6d54a4e6ed80c6a92
Gabriel Paiva <42323184+ShiroLove@users.noreply.github.com>
2025-02-03 23:55:41 -0300

    Adicionado um limpador para o botão "Jogos Alugados"

--- alugames/js/app.js
+++ alugames/js/app.js
@@ -2,8 +2,8 @@ function alterarStatus(id){
     var status = document.getElementById(`game-${id}`);
     var foto = status.querySelector('.dashboard__item__img');
     var botao = status.querySelector('.dashboard__item__button');
-
-    if (foto.classList.contains('dashboard__item__img--rented')) {
+    
+if (foto.classList.contains('dashboard__item__img--rented')) {
         if (confirm('Você Tem certeza que deseja devolver o jogo?')) {
             foto.classList.remove('dashboard__item__img--rented');
         foto.classList.add('dashboard__item__img--available');
@@ -21,14 +21,20 @@ function alterarStatus(id){
 
     }
 }
-
-function jogosAlugados () {
-    if (document.querySelectorAll('.dashboard__item__img--rented').length == 0) {
-        document.getElementById('jogos-alugados').innerHTML = ('Você não alugou nenhum jogo');
-    } else {
-        var jogos = document.querySelectorAll('.dashboard__item__img--rented').length;
-    let palavrajogos = jogos > 1 ? 'jogos' : 'jogo';
-    let mensagem = document.getElementById('jogos-alugados').innerHTML = (`Você tem ${jogos} ${palavrajogos} alugados`);
+// Botão para saber quantos jogos foram alugados
+function jogosAlugados() {
+    if (document.querySelectorAll('.dashboard__item__img--rented').length === 0) {
+        document.getElementById('jogos-alugados').innerHTML = 'Você não tem jogos alugados!';
+        } else {
+            var jogos = document.querySelectorAll('.dashboard__item__img--rented').length;
+            let palavrajogos = jogos > 1 ? 'jogos alugados' :'jogo alugado';
+            document.getElementById('jogos-alugados').innerHTML = `Você tem ${jogos} ${palavrajogos}`;
+        }
+    }
+//Função para "limpar" o botão do Jogos Alugados
+    function limparJogosAlugados() {
+        document.getElementById('jogos-alugados').innerHTML = 'Jogos Alugados';
     }
 
-}
+// Loop para limpar jogos alugados a cada 5 segundos
+    setInterval(limparJogosAlugados, 5000);
